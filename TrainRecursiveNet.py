d268ae5b40bbebc93de6a1d79658e6de8ea69739
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-03-31 22:51:37 -0400

    Added basic outline for training the R2N2 recursive net.

---
+++ TrainRecursiveNet.py
@@ -0,0 +1,12 @@
+import autograd.numpy as np
+import autograd.numpy.linalg as lg
+import autograd.numpy.random as rd
+import matplotlib.pyplot as plt
+
+from autograd import jacobian
+
+def trainRecNet():
+    pass
+
+if __name__ == '__main__':
+    trainRecNet()
